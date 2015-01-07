1ffb9cb3450d169de4e54bd02d21e7b6e38e133d
barthr <bartfokker@gmail.com>
2015-01-07 22:20:40 +0100

    politieboot

--- zandsurfer/Boat.java
+++ zandsurfer/Boat.java
@@ -25,7 +25,7 @@ public class Boat extends Actor
 
     public void setImage()
     {
-        setImage(new GreenfootImage("Boat2.png"));
+        setImage(new GreenfootImage("bootgame3.png"));
     }
 
     public void movement()
